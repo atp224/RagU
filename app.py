037f5997e06c823ce8288f914209dbb3ef3866d0
Aaron <aarontp224@yahoo.com>
2024-01-08 21:57:04 -0800

    Changed Key to be secret inside secrets.env

--- app.py
+++ app.py
@@ -4,12 +4,19 @@ import openai
 from openai import OpenAI
 import pandas as pd
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv('secrets.env')
+keys = os.getenv("API_KEY")
+
+client = openai.OpenAI(api_key=keys)
 
 file_path = 'menu.csv'
 data = pd.read_csv(file_path)
 csv_string = data.to_string()
 
-client = openai.OpenAI(api_key="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")
+client = openai.OpenAI(api_key=keys)
 
 app = Flask(__name__)
 CORS(app)
